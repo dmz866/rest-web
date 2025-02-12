b13efdf67afdeaf71db233cb9c24ed370eebedb7
David Murillo <dmz866@hotmail.com>
2025-02-12 12:15:45 -0500

    express added

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log('shi')
+import { Server } from "./presentation/server";
+import { envs } from './config/envs';
+
+const main = () => {
+    const server = new Server({
+        port: envs.PORT,
+        public_path: envs.PUBLIC_PATH,
+    });
+    server.start();
+};
+
+main();
